82c2514eba064be60b06ada97fb61406c11af234
rs-rejath <rs.rejath@ibilglobal.com>
2024-10-25 17:56:01 +0530

    Sample Commit

--- Movie2/obj/Release/net8.0/Movie2.AssemblyInfo.cs
+++ Movie2/obj/Release/net8.0/Movie2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7cb45311d869278e41622ad99c04629e87bc29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie2")]
